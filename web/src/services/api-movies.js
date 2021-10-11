033270029724cd2411e03614704ab5998c985f5b
Paloma Lozano <palomalogu9@gmail.com>
2021-10-11 18:31:58 +0200

    readme

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,17 +1,19 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (data) => {
   console.log('Se están pidiendo las películas de la app');
+
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
+
   return fetch('http://localhost:4000/movies')
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       return data;
     });
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
